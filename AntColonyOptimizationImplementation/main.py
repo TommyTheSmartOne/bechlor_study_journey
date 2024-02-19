79dfdf9f6adf4e602386b99181498aa47c3f7fd0
Your Name <minglia1@ualberta.ca>
2024-02-18 19:19:27 -0700

    Modify README.md and added some docstring for ACO implementation main

---
+++ AntColonyOptimizationImplementation/main.py
@@ -0,0 +1,8 @@
+'''
+This file is an implementation for the well known ant colony optimization, Ant Colony Optimization is an algorithm that's
+been using to resolve Travelling sales man problems and/or other problems within NP Hard domain like internet routing problem,
+Scheduling problem and so on. The algorithm will proceed in the following steps
+1. read the file that contains the dataset
+2.
+'''
+
