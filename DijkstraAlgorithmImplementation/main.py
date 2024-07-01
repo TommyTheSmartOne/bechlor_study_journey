0ac920864b35055503b8d0aa5c8b201c74a41f13
MingLiang Wang <minglia1@ualberta.ca>
2024-06-30 20:29:02 -0600

    finish the majority of RRT implementation.

--- DijkstraAlgorithmImplementation/main.py
+++ DijkstraAlgorithmImplementation/main.py
@@ -8,9 +8,33 @@ the future, the dataset must be process first such that match the above descript
 1. sort the direct path distance between the initial cities and the destination cities in ascending orders
 2. pick the next vertex with the shortest distance,
 '''
-
-# this dictionary will store all the relaxed value (shortest distance from source node) from source node to
-# destination node with key being the vertex and value
-relaxed_dict = {}
+import numpy as np
+import pandas as pd
 
 
+df = np.array(pd.read_csv("distance_of_6_cities_one_way_graph.csv"))
+print(df)
+
+
+
+
+def relaxation(current_destination_distance, current_source_distance,
+               distance_from_current_source_to_current_destination) -> int:
+    '''
+    This function will perform relaxation operation
+    :return:
+    '''
+    if current_source_distance + distance_from_current_source_to_current_destination < current_destination_distance:
+        return current_source_distance + distance_from_current_source_to_current_destination
+    else:
+        return current_destination_distance
+
+def main() -> None:
+
+
+
+
+
+source_node = 0
+destination_node = 5
+main()
